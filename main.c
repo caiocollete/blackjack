4a9fa639ab2d1ebea080caab83a1651ecceb8fa6
collete <66492069+caiocollete@users.noreply.github.com>
2023-06-23 16:02:17 -0300

    update_main.c

--- main.c
+++ main.c
@@ -42,42 +42,47 @@ int banca(int valor){
 
 
 int main() {
-    int mat[4],qntd,i=0,mao=0,banc=0;
+    int mat[4], qntd, i = 0, mao = 0, banc = 0;
 
     printf("------- BlackJack --------\n");
     printf("Para comecar jogar precisamos definir alguns parametros!\n");
     printf("Quantas pessoas irao jogar? (MAX 3)\n->");
-    scanf("%d",&qntd);
+    scanf("%d", &qntd);
+    while (qntd > 3) {
+        printf("Quantidade de jogadores nao suportada!\n");
+        printf("Digite novamente quantas pessoas irao jogar? (MAX 3)\n->");
+        scanf("%d", &qntd);
+    }
     system("cls");
     printf("------- BlackJack --------\n");
-    printf("Ok, %d irao jogar!\n",qntd);
+    printf("Ok, %d irao jogar!\n", qntd);
     printf("Para comecar precione qualquer tecla!\n");
     system("pause");
     system("cls");
 
-    while(i<qntd) {
+    while (i < qntd) {
         printf("\n-------------\nVez do jogador %d\n-------------\n", i + 1);
         mat[i] = blackjack(mao);
         i++;
     }
 
-    mat[4]= banca(banc);
+    mat[4] = banca(banc);
 
-    for(i=0;i<qntd;i++){
-        if(mat[4]>21 && mat[i]<=21){
-            printf("JOGADOR [%d]: O jogador ganhou!\n",i+1);
+    for (i = 0; i < qntd; i++) {
+        if (mat[4] > 21 && mat[i] <= 21) {
+            printf("JOGADOR [%d]: O jogador ganhou!\n", i + 1);
         }
-        else if(mat[4]==21){
-            printf("JOGADOR [%d]: A banca ganhou!\n",i+1);
+        else if(mat[i]>21){
+            printf("JOGADOR [%d]: A banca ganhou!\n", i + 1);
         }
-        else if(mat[4]<=21 && mat[i]<mat[4]){
-            printf("JOGADOR [%d]: A banca ganhou!\n",i+1);
+        else if (mat[4] == 21) {
+            printf("JOGADOR [%d]: A banca ganhou!\n", i + 1);
+        } else if (mat[4] <= 21 && mat[i] < mat[4]) {
+            printf("JOGADOR [%d]: A banca ganhou!\n", i + 1);
+        } else if (mat[4] == mat[i]) {
+            printf("JOGADOR [%d]: A banca ganhou!\n", i + 1);
         }
-        else if(mat[4] == mat[i]){
-            printf("JOGADOR [%d]: A banca ganhou!\n",i+1);
+
     }
-
-
-
     return 0;
 }
